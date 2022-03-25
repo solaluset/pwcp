f96532ca500b18c2cf660ea85af040e0f542406f
unknown <hapzazad@gmail.com>
2022-03-25 16:10:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pwcp',
-    version='0.1',
+    version='0.2b',
     packages=['pwcp'],
     install_requires=['pcpp@git+https://github.com/Krutoy-4el/pcpp.git'],
     entry_points={
