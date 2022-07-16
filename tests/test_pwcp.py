1620fc9d787b9a0a6c53f7a1d1b8c825fa607401
unknown <hapzazad@gmail.com>
2022-07-16 18:12:03 +0300

    added automatic tests

---
+++ tests/test_pwcp.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from io import StringIO
+from unittest.mock import patch
+from subprocess import STDOUT, CalledProcessError, check_output
+
+import pytest
+
+from pwcp import main
+
+
+def test_regular_file():
+    with patch("sys.stdout", new=StringIO()):
+        main(["tests/hello.py"])
+        assert sys.stdout.getvalue() == "Hello world! (python)\n"
+
+
+def test_ppy_file():
+    with patch("sys.stdout", new=StringIO()):
+        main(["tests/hello.ppy"])
+        assert sys.stdout.getvalue() == "Hello world!\n"
+
+
+def test_imports():
+    main(["tests/test_modules.ppy"])
+    assert sys.modules["hello"].__file__ == os.path.join(
+        os.path.abspath("tests"), "hello.ppy"
+    )
+    del sys.modules["hello"]
+
+
+def test_py_import():
+    main(["--prefer-py", "tests/test_modules.ppy"])
+    assert sys.modules["hello"].__file__ == os.path.join(
+        os.path.abspath("tests"), "hello.py"
+    )
+    del sys.modules["hello"]
+
+
+def test_syntax_error():
+    with pytest.raises(CalledProcessError) as ctx:
+        check_output(["pwcp", "tests/syntax_error.ppy"], stderr=STDOUT)
+    assert ctx.value.output.splitlines()[1].strip() == b'print("hello")!'
+
+
+def test_type_error():
+    with pytest.raises(CalledProcessError) as ctx:
+        check_output(["pwcp", "tests/type_error.ppy"], stderr=STDOUT)
+    assert ctx.value.output.splitlines()[-2].strip() == b"print('1' + 1)"
