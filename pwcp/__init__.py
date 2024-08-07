8c6b711fabe01d4a6b262b088c5d708a779b976b
solaluset <60041069+solaluset@users.noreply.github.com>
2024-08-07 15:46:24 +0300

    Expose hooks.install as a public interface

--- pwcp/__init__.py
+++ pwcp/__init__.py
@@ -1,5 +1,12 @@
-__all__ = ("main", "main_with_params", "__version__", "add_file_extension")
+__all__ = (
+    "main",
+    "main_with_params",
+    "__version__",
+    "add_file_extension",
+    "install",
+)
 
 from .runner import main, main_with_params
 from .version import __version__
 from .config import add_file_extension
+from .hooks import install
