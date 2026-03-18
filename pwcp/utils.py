be676b4d3058a2ddba58545f49af9ec02837ed7e
solaluset <60041069+solaluset@users.noreply.github.com>
2026-03-18 09:55:02 +0200

    Refactor import path finder

--- pwcp/utils.py
+++ pwcp/utils.py
@@ -4,7 +4,6 @@ import warnings
 from typing import Callable, Optional, Type
 from traceback import print_exception
 from types import ModuleType, TracebackType
-from importlib import util
 from importlib.machinery import all_suffixes
 
 from .errors import PreprocessorError
@@ -56,24 +55,3 @@ def is_package(module_name: str) -> bool:
 def py_from_ppy_filename(filename: str) -> str:
     file_path = os.path.splitext(filename)[0]
     return file_path + ".py"
-
-
-def import_module_copy(name: str):
-    orig_module = sys.modules.pop(name, None)
-    spec = util.find_spec(name)
-    if orig_module:
-        sys.modules[name] = orig_module
-
-    module = util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    return module
-
-
-def create_sys_clone():
-    sys_clone = ModuleType("sys")
-    vars(sys_clone).update(vars(sys))
-    sys_clone.path_hooks = []
-    sys_clone.path_importer_cache = {}
-
-    return sys_clone
