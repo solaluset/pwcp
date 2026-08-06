4525daaa53a97e33a534b61d00b3a03a088b92d5
Sóla Łuset <60041069+solaluset@users.noreply.github.com>
2026-08-07 02:39:16 +0300

    Start rewriting internal processing callbacks

---
+++ pwcp/preprocessing_funcs.py
@@ -0,0 +1,51 @@
+from typing import Any, TextIO, Tuple, Union
+
+from .config import HOOKS
+from .utils import py_from_ppy_filename
+
+preprocessed_files = {}
+
+
+def preprocess(
+    src: Union[str, TextIO], filename: str, data: dict
+) -> tuple[str, dict]:
+    if not isinstance(src, str):
+        src = src.read()
+
+    # indicate that we started preprocessing
+    preprocessed_files[filename] = None
+
+    result_data = {}
+    for hook in HOOKS:
+        if hook.name not in data:
+            data[hook.name] = hook.create_state()
+        src, hook_data = hook.process(src, filename, data[hook.name])
+        result_data[hook.name] = hook_data
+
+    # save preprocessed file to display actual SyntaxError
+    preprocessed_files[filename] = src
+
+    return src, result_data
+
+
+def preprocess_file(
+    filename: str, save_files: bool = False
+) -> Tuple[str, dict]:
+    with open(filename) as f:
+        res, deps = preprocess(f, filename, {})
+    if save_files:
+        with open(py_from_ppy_filename(filename), "w") as f:
+            f.write(res)
+    return res, deps
+
+
+def maybe_preprocess(src: Any, filename: str, data: dict) -> str:
+    if isinstance(src, bytes):
+        src = src.decode()
+    if isinstance(src, str):
+        # this is essential for interactive mode
+        has_newline = src.endswith("\n")
+        src, _ = preprocess(src, filename, data)
+        if not has_newline:
+            src = src.rstrip("\n")
+    return src
--- pwcp/preprocessor.py
+++ pwcp/preprocessor.py
@@ -1,16 +1,10 @@
 from io import StringIO
 from linecache import getline
-from importlib.machinery import SOURCE_SUFFIXES
-from typing import Any, Callable, Optional, TextIO, Tuple, Union
+from typing import Optional, TextIO
 
 from pypp import Preprocessor
 
-from .config import FILE_EXTENSIONS
-from .utils import py_from_ppy_filename
 from .errors import PreprocessorError
-
-
-preprocessed_files = {}
 
 
 class PyPreprocessor(Preprocessor):
@@ -39,89 +33,24 @@ class PyPreprocessor(Preprocessor):
         self.included_files.append(includepath)
         return super().on_file_open(is_system_include, includepath)
 
+    def preprocess(self, source: str, filename: str) -> tuple[str, list[str]]:
+        self.parse(source, filename)
 
-PreprocessingFunction = Callable[[str, str, PyPreprocessor], str]
+        out = StringIO()
+        try:
+            self.write(out)
+        except SyntaxError:
+            raise
+        except Exception as e:
+            msg = "internal preprocessor error"
+            last = self.lastdirective
+            if last:
+                msg += f" at around {last.source}:{last.lineno}"
+            raise PreprocessorError(msg) from e
 
+        if self.return_code != 0:
+            raise PreprocessorError(
+                f"preprocessor exit code is not zero: {self.return_code}"
+            )
 
-def _preprocess(src: str, filename: str, preprocessor: PyPreprocessor) -> str:
-    preprocessor.parse(src, filename)
-
-    out = StringIO()
-    try:
-        preprocessor.write(out)
-    except SyntaxError:
-        raise
-    except Exception as e:
-        msg = "internal preprocessor error"
-        last = preprocessor.lastdirective
-        if last:
-            msg += f" at around {last.source}:{last.lineno}"
-        raise PreprocessorError(msg) from e
-
-    if preprocessor.return_code != 0:
-        raise PreprocessorError(
-            f"preprocessor exit code is not zero: {preprocessor.return_code}"
-        )
-
-    return out.getvalue()
-
-
-def set_preprocessing_function(
-    func: PreprocessingFunction,
-) -> PreprocessingFunction:
-    global _preprocess
-
-    prev_func = _preprocess
-    _preprocess = func
-
-    return prev_func
-
-
-def preprocess(
-    src: Union[str, TextIO], filename: str, p: Optional[PyPreprocessor] = None
-):
-    if not isinstance(src, str):
-        src = src.read()
-
-    if p is None:
-        # always enable preprocessing of ppy files
-        if filename.endswith(tuple(FILE_EXTENSIONS)):
-            disabled = False
-        # but disable other Python files
-        elif filename.endswith(tuple(SOURCE_SUFFIXES)):
-            disabled = True
-        else:
-            disabled = None
-        p = PyPreprocessor(disabled=disabled)
-
-    # indicate that we started preprocessing
-    preprocessed_files[filename] = None
-
-    # save preprocessed file to display actual SyntaxError
-    result = preprocessed_files[filename] = _preprocess(src, filename, p)
-    return result, p.included_files
-
-
-def preprocess_file(
-    filename: str, save_files: bool = False
-) -> Tuple[str, list]:
-    with open(filename) as f:
-        res, deps = preprocess(f, filename)
-    if save_files:
-        with open(py_from_ppy_filename(filename), "w") as f:
-            f.write(res)
-    return res, deps
-
-
-def maybe_preprocess(
-    src: Any, filename: str, preprocessor: Optional[PyPreprocessor] = None
-):
-    if isinstance(src, bytes):
-        src = src.decode()
-    if isinstance(src, str):
-        # this is essential for interactive mode
-        has_newline = src.endswith("\n")
-        src, _ = preprocess(src, filename, preprocessor)
-        if not has_newline:
-            src = src.rstrip("\n")
-    return src
+        return out.getvalue(), self.included_files
