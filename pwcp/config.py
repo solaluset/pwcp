05b659ce5c750c2e53d34a4f8e9beeb4bf8e070c
Sóla Łuset <60041069+solaluset@users.noreply.github.com>
2026-08-07 02:45:06 +0300

    Move hooks to separate file

--- pwcp/config.py
+++ pwcp/config.py
@@ -1,42 +1,8 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-from .preprocessor import PyPreprocessor
+from .hooks import PreprocessorHooks, PWCPHooks
 
 FILE_EXTENSIONS = [".ppy"]
-HOOKS: list["PreprocessorHooks"] = []
+HOOKS: list[PreprocessorHooks] = []
 
 add_file_extension = FILE_EXTENSIONS.append
 add_hook = HOOKS.append
-
-
-class PreprocessorHooks(ABC):
-    def __init__(self, name: str):
-        self.name = name
-
-    @abstractmethod
-    def create_state(self) -> Any:
-        raise NotImplementedError
-
-    @abstractmethod
-    def process(
-        self, source: str, filename: str, state: Any
-    ) -> tuple[str, Any]:
-        raise NotImplementedError
-
-
-class PWCPHooks(PreprocessorHooks):
-    def __init__(self):
-        super().__init__("pwcp")
-
-    def create_state(self) -> PyPreprocessor:
-        return PyPreprocessor(disabled=False)
-
-    def process(
-        self, source: str, filename: str, preprocessor: PyPreprocessor
-    ) -> tuple[str, list[str]]:
-        preprocessor.disabled = filename.endswith(".py")
-        return preprocessor.preprocess(source, filename)
-
-
 add_hook(PWCPHooks())
