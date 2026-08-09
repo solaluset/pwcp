740cce1fe13e322be2b2f104fead9b49cd3c0ea1
Sóla Łuset <60041069+solaluset@users.noreply.github.com>
2026-08-09 18:46:10 +0300

    Allow choosing default preprocessor state for file extension

--- pwcp/config.py
+++ pwcp/config.py
@@ -1,11 +1,21 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from importlib.machinery import SOURCE_SUFFIXES
 
 if TYPE_CHECKING:
     from .hooks import PreprocessorHooks
 
-FILE_EXTENSIONS: list[str] = []
+FILE_EXTENSIONS: dict[str, bool] = {}
 HOOKS: list[PreprocessorHooks] = []
 
-add_file_extension = FILE_EXTENSIONS.append
+
+def add_file_extension(extension: str, /, *, preprocess: bool = True):
+    FILE_EXTENSIONS[extension] = not preprocess
+
+
 add_hook = HOOKS.append
+
+
+add_file_extension(".ppy")
+for ext in SOURCE_SUFFIXES:
+    add_file_extension(ext, preprocess=False)
