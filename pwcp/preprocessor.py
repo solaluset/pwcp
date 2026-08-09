a46198fad61fce69d3f3981d27c202fc6049bd8a
solaluset <60041069+solaluset@users.noreply.github.com>
2026-08-09 09:05:27 +0300

    Merge preprocessing_funcs.py into preprocessor.py

--- pwcp/preprocessor.py
+++ pwcp/preprocessor.py
@@ -1,9 +1,11 @@
-from io import StringIO
+from io import BytesIO, StringIO
 from linecache import getline
-from typing import Optional, TextIO
+from tokenize import detect_encoding
+from typing import Any, Optional, TextIO
 
 from pypp import Preprocessor
 
+from .config import HOOKS
 from .errors import PreprocessorError
 
 
@@ -50,3 +52,36 @@ class PyPreprocessor(Preprocessor):
             )
 
         return out.getvalue(), self.included_files
+
+
+preprocessed_files: dict[str, str] = {}
+
+
+def preprocess(src: str, filename: str, data: dict) -> tuple[str, dict]:
+    # indicate that we started preprocessing
+    preprocessed_files[filename] = None
+
+    result_data = {}
+    for hook in HOOKS:
+        if hook.name not in data:
+            data[hook.name] = hook.create_state()
+        src, hook_data = hook.process_source(src, filename, data[hook.name])
+        result_data[hook.name] = hook_data
+
+    # save preprocessed file to display actual SyntaxError
+    preprocessed_files[filename] = src
+
+    return src, result_data
+
+
+def maybe_preprocess(src: Any, filename: str, data: dict) -> str:
+    if isinstance(src, bytes):
+        encoding, _ = detect_encoding(BytesIO(src).readline)
+        src = src.decode(encoding)
+    if isinstance(src, str):
+        # this is essential for interactive mode
+        has_newline = src.endswith("\n")
+        src, _ = preprocess(src, filename, data)
+        if not has_newline:
+            src = src.rstrip("\n")
+    return src
